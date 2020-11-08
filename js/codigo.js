bf689d134719fcbf7650b242501d604eb5bcd0d2
keysel <keyselharewood@hotmail.com>
2020-11-08 12:45:41 -0430

    Página informátiva covid 19

---
+++ js/codigo.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$('.ir').click(function(){
+		$('body, html').animate({
+			scrollTop:'0px'
+		}, 300);
+	});
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 100){
+			$('.ir').slideDown(300);
+		}else{
+			$('.ir').slideUp(300);
+		}
+	})
+})
